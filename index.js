43d897364e4d553c7d01e38c309ac39f0539e00b
CESAR RODRIGO RAMIREZ FLEITAS <cesarrodrigoramirez@gmail.com>
2024-10-30 15:33:57 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import cors from 'cors'
+import path from 'path'
+import mongoose from 'mongoose'
+import * as dotenv from 'dotenv'
+import router from './router/index'
+
+dotenv.config()
+
+//Conexion a la BD
+mongoose.Promise = global.Promise
+const url_db= process.env.URL_DB;
+mongoose.connect(url_db)
+.then(mongoose=> console.log("Conectado a la DB"))
+.catch(err => console.log(err))
+
+const app = express()
+const puerto = process.env.PORT || 3000
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/api/', router)
+app.set('port', puerto)
+
+app.listen(app.get('port'), ()=> {
+    console.log(`EL SERVIDOR SE ESTA EJECUTANDO EN EL PUERTO ${puerto}`)
+})
