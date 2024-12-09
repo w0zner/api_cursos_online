3d2c5bd436d04ee8be7d6a4d13c1bb43dd54da0b
cesar_ramirez <cesar_ramirez@hacienda.gov.py>
2024-12-09 15:50:36 -0300

    Se termina de agregar endpoints para usuarios

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,6 +1,8 @@
 import models from "../models"
 import bcrypt from "bcryptjs"
 import token from "../services/token"
+import fs from 'fs'
+import path from 'path'
 
 const register = async(req, res)=> {
     try {
@@ -135,7 +137,7 @@ const getUsers = async(req, res)=> {
     } catch (error) {
         console.log(error)
         res.status(500).json({
-            msg: "Error inesperado en user.getUsers",
+            msg: "Error inesperado al listar usuarios",
         })
     }
 }
@@ -175,9 +177,113 @@ const register_admin = async(req, res) => {
     }
 }
 
+const getImagen = (req, res) => {
+    try {
+        var img  = req.params["img"]
+        if(!img){
+            res.status(500).send({
+                message: 'Error inesperado al obtener la imagen'
+            })
+        } else {
+            fs.stat('./uploads/user/' + img, function(error) {
+                if(!error) {
+                    let path_img = './uploads/user/' + img
+                    res.status(200).sendFile(path.resolve(path_img))
+                } else {
+                    let path_img = './uploads/default.jpg'
+                    res.status(200).sendFile(path.resolve(path_img))
+                }
+            })
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({ message: 'Error inesperado' })
+    }
+}
+
+const update =  async(req, res) => {
+    try {
+
+        const { email } = req.body.email
+        console.log(email)
+
+        const user_exist= await models.User.findOne({email: req.body.email, _id: {$ne: req.body._id}})
+
+        if(user_exist) {
+            res.status(403).json({
+                msg: "Ya existe un usuario registrado con el email: " + req.body.email
+            })
+        }
+
+        if(req.body.password) {
+            //Encriptación de contraseña
+            req.body.password= await bcrypt.hash(req.body.password, 10);
+        }
+        
+        if(req.files && req.files.avatar) {
+            var img_path = req.files.avatar.path
+            var name = img_path.split("\\")
+            var avatar_name = name[2]
+            req.name.avatar = avatar_name
+        }
+
+        const User = await models.User.findByIdAndUpdate({_id: req.body._id}, req.body)    
+        res.status(200).json({
+            msg: "Registro actualizado   exitosamente",
+            user: User
+        })
+    } catch(error) {
+        console.log(error)
+        res.status(500).send({ message: 'Error inesperado' })
+    }
+}
+
+const listUsers = async(req, res)=> {
+    try {
+        const search = req.query.search
+
+        let USERS = await models.User.find({
+            $or: [
+                {'name': new RegExp(search, 'i')},
+                {'surname': new RegExp(search, 'i')},
+                {'email': new RegExp(search, 'i')}
+            ]
+        }).sort({'createdAt': -1})
+
+        res.status(200).json(
+            {users: USERS}
+        )
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            msg: "Error inesperado al listar usuarios",
+        })
+    }
+}
+
+const remove = async (req, res) => {
+    try {
+        const _id= req.params["id"]
+
+        const  User= await models.User.findByIdAndDelete({_id: _id})
+        
+        res.status(200).json({message: "Usuario eliminado correctamente."})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            msg: "Error inesperado al eliminar usuarios",
+        })
+    }
+}
+
 export default {
     getUsers,
     register,
     login,
-    login_admin
+    login_admin,
+    register_admin,
+    getImagen,
+    update,
+    listUsers,
+    remove
 }
--- router/User.js
+++ router/User.js
@@ -5,13 +5,20 @@ import multiparty from 'connect-multiparty'
 var path = multiparty({uploadDir: './uploads/user'})
 
 const router = routerx()
-
+//prueba
 router.get("/all", userController.getUsers)
+
+router.get("/list", path, userController.listUsers)
 router.post("/register", userController.register)
+router.post("/update", path, userController.update)
+router.delete("/delete/:id", path, userController.remove)
+router.get("/imagen-usuario/:img", userController.getImagen)
+
 router.post("/login", userController.login)
-router.post("/login-admin", userController.login_admin)
 
 //ADMIN
 router.post("/register-admin", path, userController.register_admin)
+router.post("/login-admin", userController.login_admin)
+
 
 export default router
